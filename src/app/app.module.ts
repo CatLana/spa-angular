9ddd8cfefbee6c2a55531047c541c14084c1795e
CatLana <ssavchenko8@gmail.com>
2017-11-04 17:37:11 +0000

    Implement drag and drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { FormDataService } from './form-data.service';
+import {DndModule} from 'ng2-dnd';
+
 
 @NgModule({
-  imports: [BrowserModule, HttpModule],
+  imports: [BrowserModule, HttpModule,  DndModule.forRoot()],
   providers: [FormDataService],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
